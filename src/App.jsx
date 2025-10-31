b6da637078e47804183a97c40329c9c0c0b434bf
Cherry-Machado <cherry5467@gmail.com>
2025-10-31 13:57:33 -0300

    Finished all the Todo list in one file

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,35 @@ export default function App() {
 
   function handleSubmit(e) {
     e.preventDefault()
+
     setTodos(currentTodos => {
       return [
         ...currentTodos,
         { id: crypto.randomUUID(), title: newItem, completed: false }
       ]
     })
+
+    setNewItem("")
   }
+
+  function toggleTodo(id, completed) {
+    setTodos(currentTodos => {
+      return currentTodos.map(todo => {
+        if (todo.id === id) {
+          return { ...todo, completed }
+        }
+        return todo
+      })
+    })
+  }
+
+  function deleteTodo(id) {
+    setTodos(currentTodos => {
+      return currentTodos.filter(todo => todo.id !== id)
+    })
+  }
+
+
   return (
     <>
       <form onSubmit={handleSubmit} className="new-item-form">
@@ -31,14 +53,21 @@ export default function App() {
       </form>
       <h1 className="header">Todo List</h1>
       <ul className="list">
+        {todos.length === 0 && "No todos found"}
         {todos.map(todo => {
           return (
             <li key={todo.id}>
               <label>
-                <input type="checkbox" checked={todo.completed} />
+                <input type="checkbox" checked={todo.completed} 
+                onChange={e => toggleTodo(todo.id, e.target.checked)} />
                 {todo.title}
               </label>
-              <button className="btn btn-danger">Delete</button>
+              <button 
+                onClick={() => deleteTodo(todo.id)} 
+                className="btn btn-danger"
+              >
+                Delete
+              </button>
             </li>
           )
         })}
