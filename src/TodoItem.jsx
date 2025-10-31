0f7d11a864d18fcc0b76cadf5a6224b252817ea6
Cherry-Machado <cherry5467@gmail.com>
2025-10-31 17:24:06 -0300

    Adding localStorage to the project

--- src/TodoItem.jsx
+++ src/TodoItem.jsx
@@ -1,18 +1,20 @@
 export function TodoItem({ completed, id, title, toggleTodo, deleteTodo }) {
-    <li>
-        <label>
-        <input 
-            type="checkbox" 
-            checked={completed} 
-            onChange={e => toggleTodo(id, e.target.checked)} 
-        />
-        {title}
-        </label>
-        <button 
-        onClick={() => deleteTodo(id)} 
-        className="btn btn-danger"
-        >
-        Delete
-        </button>
-    </li>
+    return (
+        <li>
+            <label>
+            <input 
+                type="checkbox" 
+                checked={completed} 
+                onChange={e => toggleTodo(id, e.target.checked)} 
+            />
+            {title}
+            </label>
+            <button 
+            onClick={() => deleteTodo(id)} 
+            className="btn btn-danger"
+            >
+            Delete
+            </button>
+        </li>
+    )
 }
